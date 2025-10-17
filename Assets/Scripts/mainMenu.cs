3640a7c3f987cec0eb6bd879892e8cad441fea53
Cecil <cecil.creates@gmail.com>
2025-10-16 22:49:01 -0400

    merge + menu functions
    merged mathews camera screen into my branch. did not touch scene. created functionality for main menu, all screens work in flow with no dead ends. options menu functions for a later date, game manager later date.

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//CECIL SCRIPT
+public class mainMenu : MonoBehaviour
+{
+    public GameObject mainMenuObject; //initializing objects in code for inspector attachment
+    public GameObject optionsMenu;
+    public GameObject creditsMenu;
+
+    void Start(){ // only main menu is active on start not the other canvases.
+    optionsMenu.SetActive(false);
+    creditsMenu.SetActive(false);
+    }
+
+    public void playGame(){
+        SceneManager.LoadScene(1);
+    }
+//prevents overlap
+    public void options(){// show only options screen
+         optionsMenu.SetActive(true);
+          mainMenuObject.SetActive(false);
+    }
+
+    public void credits(){//ditto
+    creditsMenu.SetActive(true);
+          mainMenuObject.SetActive(false);
+
+    }
+
+    public void backButton(){ // no matter which back button, sends back to main screen.
+  optionsMenu.SetActive(false);
+    creditsMenu.SetActive(false);
+    mainMenuObject.SetActive(true);
+
+    }
+
+    public void quit(){
+        Application.Quit();
+    }
+}
