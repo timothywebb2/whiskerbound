ac45df25f167f19a91304516d98325d93b32dd20
Matthew Nepywoda <ma907122@ucf.edu>
2025-10-21 16:48:18 -0400

    removed iso camera

--- Assets/Scripts/ProtoMovement.cs
+++ Assets/Scripts/ProtoMovement.cs
@@ -1,17 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro;
 using Unity.Cinemachine;
 public class ProtoMovement : MonoBehaviour
 {
     public InputActionAsset InputActions;
-    public TMP_Text cameraType;
-    public CinemachineCamera isoCamera;
     public CinemachineCamera frontCamera;
     private InputAction m_move;
     private Vector2 m_moveAmt;
-    private InputAction m_switch;
-    private bool isIsometric = true;
     private float sensitivity = 50f;
     private void OnEnable()
     {
@@ -26,24 +21,6 @@ public class ProtoMovement : MonoBehaviour
     private void Awake()
     {
         m_move = InputSystem.actions.FindAction("Move");
-        m_switch = InputSystem.actions.FindAction("Jump");
-
-        m_switch.performed += _ =>
-        {
-            if (isIsometric)
-            {
-                isoCamera.Priority = 10;
-                frontCamera.Priority = 0;
-                cameraType.text = "Isometric";
-            }
-            else
-            {
-                isoCamera.Priority = 0;
-                frontCamera.Priority = 10;
-                cameraType.text = "Front";
-            }
-            isIsometric = !isIsometric;
-        };
     }
     
     void Update()
