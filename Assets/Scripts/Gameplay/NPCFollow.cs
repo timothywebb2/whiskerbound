e12b275ec873a45c67d326045f477dee4723cf82
Matthew Nepywoda <ma907122@ucf.edu>
2026-02-27 15:04:59 -0500

    cleric walking animations

--- Assets/Scripts/Gameplay/NPCFollow.cs
+++ Assets/Scripts/Gameplay/NPCFollow.cs
@@ -14,12 +14,15 @@ public class NPCFollow : MonoBehaviour
     public float fastSpeed; // fast NPC walking speed to catch up
     public float fastDistance;
     public float removeDistance; // margin of error when NPC travels to sample (smaller number = more accurate)
+    private Animator animator;
 
     void Start()
     {
         sampleTime = Time.time;
         followCharacterPositions.Add(followCharacter.position);
         followSpeed = fastSpeed;
+
+        animator = this.GetComponent<Animator>();
     }
 
     void Update()
@@ -40,8 +43,22 @@ public class NPCFollow : MonoBehaviour
         else
             followSpeed = baseSpeed;
         
+        int xDir = 0;
+        int yDir = 0;
+
         if(Vector3.Distance(transform.position, followCharacter.position) > distanceFromCharacter) // if the current distance between NPC and character is above max
         {
+
+            if(transform.position.z > /*followCharacterPositions[0].z*/ followCharacter.position.z) //walking to the right, +x
+                xDir = 1;
+            else if(transform.position.z < /*followCharacterPositions[0].z*/ followCharacter.position.z) //to the left, -x
+                xDir = -1;
+
+            if(transform.position.x > /*followCharacterPositions[0].x*/ followCharacter.position.x) //forward, -y
+                yDir = -1;
+            else if(transform.position.x < /*followCharacterPositions[0].x*/ followCharacter.position.x) //backward, +y
+                yDir = 1;
+            
             transform.position = Vector3.MoveTowards(transform.position, followCharacterPositions[0], Time.deltaTime * followSpeed);
 
             if(Vector3.Distance(transform.position, followCharacterPositions[0]) < removeDistance && followCharacterPositions.Count > 1)
@@ -49,5 +66,8 @@ public class NPCFollow : MonoBehaviour
                 followCharacterPositions.RemoveAt(0);
             } 
         }
+
+        animator.SetInteger("XDirection", xDir);
+        animator.SetInteger("YDirection", yDir);
     }
 }
