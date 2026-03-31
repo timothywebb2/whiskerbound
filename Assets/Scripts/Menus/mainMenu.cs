8dea9502b8aa71aed1b5b2406ac6a03b8b3989b8
Cecil <cecil.creates@gmail.com>
2026-03-31 15:28:36 -0400

    changelog

--- Assets/Scripts/Menus/mainMenu.cs
+++ Assets/Scripts/Menus/mainMenu.cs
@@ -11,9 +11,6 @@ public class mainMenu : MonoBehaviour
     public GameObject creditsMenu;
     public GameObject controlsMenu;
 
-    public GameObject KeyScreen;
-    public GameObject XboxScreen;
-
     public GameObject blackScreen;
 
     void Start(){ // only main menu is active on start not the other canvases.
@@ -36,37 +33,28 @@ public class mainMenu : MonoBehaviour
     //prevents overlap
     public void options(){// show only options screen
          optionsMenu.SetActive(true);
-          mainMenuObject.SetActive(false);
+          //mainMenuObject.SetActive(false);
     }
 
     public void credits(){//ditto
     creditsMenu.SetActive(true);
-          mainMenuObject.SetActive(false);
+          //mainMenuObject.SetActive(false);
 
     }
 
 public void controls(){//ditto but close options screen
      controlsMenu.SetActive(true);
      optionsMenu.SetActive(false);
-        mainMenuObject.SetActive(false);
+        //mainMenuObject.SetActive(false);
 }
 
     public void backButton(){ // no matter which back button, sends back to main screen.
   optionsMenu.SetActive(false);
     creditsMenu.SetActive(false);
     controlsMenu.SetActive(false);
-    mainMenuObject.SetActive(true);
+    //mainMenuObject.SetActive(true);
     }
 
-public void xbox(){
-    XboxScreen.SetActive(true);
-    KeyScreen.SetActive(false);
-}
-
-public void key(){
-    KeyScreen.SetActive(true);
-    XboxScreen.SetActive(false);
-}
     public void quit(){
         Application.Quit();
     }
